283cd8c3c7c3e4e51f191593e58528d2478663ac
Anton Buensuceso <anton.buens@gmail.com>
2025-02-13 09:24:47 -0800

    Initial sharing of project

---
+++ diamond.java
@@ -0,0 +1,20 @@
+/**
+ * Description: This program uses 7 print statements to output a diamond
+ * 
+ * @author Anton Buensuceso
+ * @version v1.0
+ * @since 20250213
+ */
+
+public class diamond {
+	public static void main(String[] args) {
+		System.out.println("   *");
+		System.out.println("  ***");
+		System.out.println(" *****");
+		System.out.println("*******");
+		System.out.println(" *****");
+		System.out.println("  ***");
+		System.out.println("   *");
+
+	}
+}
---
+++ progChall3.java
@@ -0,0 +1,19 @@
+/**
+ * Description: This program outputs name, major and random fact using one 
+ * print statement using escape character \n
+ * 
+ * @author Anton Buensuceso
+ * @version v1.0
+ * @since 20250213
+ */
+
+
+
+public class progChall3 {
+	public static void main(String[] args) {
+		System.out.print("Anton Buensuceso\n"
+				+ "Undeclared\n"
+				+ "I love eating at AYCE places\n");
+	}
+}
+
